4d5090abbde2ad8da9a56279a90d139a0da53a9f
Devaraj <devarajperayil327@gmail.com>
2023-10-13 12:09:04 +0530

    css

--- server/router.ts
+++ server/router.ts
@@ -2,8 +2,8 @@ import { Router, Request, Response, NextFunction } from "express";
 
 const router = Router();
 
-router.get("/",(req: Request, res: Response, next: NextFunction) => {
-    
+router.get("/",(_req: Request, res: Response) => {
+    return res.json("GET API");
 });
 
 export default router;
