a6d8141b53ffb398969c446a08fa3be8559f86d1
Devaraj <devarajperayil327@gmail.com>
2023-11-01 11:27:35 +0530

    mobile header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 "use client"
+import { useState } from "react";
 
 import "./style.scss";
 import Header from "@app/components/header/header";
+import MobileHeader from "@app/components/mobile-header/mobile-header";
 import Circle from "@app/components/circle/circle";
 
 export default function App() {
+  const [mobileToggle, setMobileTogge] = useState<Boolean>(false);
   return (
     <main className="min-h-screen">
       <Circle />
-      <Header />
+      <Header mobileToggle={mobileToggle} setMobileTogge={setMobileTogge}/>
+      <MobileHeader />
       <div style={{ height: "200vh" }}>
       </div>
     </main>
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,16 @@
 "use client"
 import Image from "next/image";
-import { CSSProperties, useEffect, useRef } from "react";
+import { useEffect, useRef, SetStateAction } from "react";
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import PhoneIcon from '@mui/icons-material/Phone';
 
 import "./style.scss";
 import novaviLogo from "@app/assets/novavi-logo.png";
 
-export default function Header(props: CSSProperties) {
+export default function Header({ mobileToggle, setMobileTogge}) {
+    function openMenu() {
+
+    }
  const scrollRef = useRef<HTMLDivElement>(null!);
  useEffect(() => {
   let body = document.querySelector("body");
@@ -20,7 +23,7 @@ export default function Header(props: CSSProperties) {
   });
  }, []);
  return (
-  <div className="header-wrapper" ref={scrollRef} style={props}>
+  <div className="header-wrapper" ref={scrollRef}>
    <div className="container">
     <div className="row">
      <div className="col">
@@ -148,7 +151,7 @@ export default function Header(props: CSSProperties) {
       <a href="contact-us/index.html" className="contact-btn">
        Contact Us
       </a>
-      <button className="hamburgermenu">
+      <button className="hamburgermenu" onClick={openMenu}>
        <span></span>
        <span></span>
        <span></span>
@@ -157,152 +160,5 @@ export default function Header(props: CSSProperties) {
     </div>
    </div>
   </div>
-  // <Test />
- );
-}
-
-
-
-
-
-
-
-function Test() {
- return (
-  <div className="header-wrapper">
-   <div className="container">
-    <div className="row">
-     <div className="col-lg-4 col-md-4 col-9">
-      <a href="index.html" className="header-logo">
-       <img
-        src="wp-content/themes/novavi/assets/img/novavi-logo.png"
-        alt="Novavi Logo"
-       />
-      </a>
-     </div>
-     <div className="col-lg-4">
-      <div id="mega-menu-wrap-primary" className="mega-menu-wrap">
-       <div className="mega-menu-toggle">
-        <div className="mega-toggle-blocks-left" />
-        <div className="mega-toggle-blocks-center" />
-        <div className="mega-toggle-blocks-right">
-         <div
-          className="mega-toggle-block mega-menu-toggle-animated-block mega-toggle-block-0"
-          id="mega-toggle-block-0"
-         >
-          <button
-           aria-label="Toggle Menu"
-           className="mega-toggle-animated mega-toggle-animated-slider"
-           type="button"
-           aria-expanded="false"
-          >
-           <span className="mega-toggle-animated-box">
-            <span className="mega-toggle-animated-inner" />
-           </span>
-          </button>
-         </div>
-        </div>
-       </div>
-       <ul
-        id="mega-menu-primary"
-        className="mega-menu max-mega-menu mega-menu-horizontal mega-no-js"
-        data-event="hover_intent"
-        data-effect="fade_up"
-        data-effect-speed={200}
-        data-effect-mobile="disabled"
-        data-effect-speed-mobile={0}
-        data-mobile-force-width="false"
-        data-second-click="go"
-        data-document-click="collapse"
-        data-vertical-behaviour="standard"
-        data-breakpoint={768}
-        data-unbind="true"
-        data-mobile-state="collapse_all"
-        data-hover-intent-timeout={300}
-        data-hover-intent-interval={100}
-       >
-        <li
-         className="mega-menu-item mega-menu-item-type-custom mega-menu-item-object-custom mega-menu-item-has-children mega-align-bottom-left mega-menu-flyout mega-disable-link mega-menu-item-103"
-         id="mega-menu-item-103"
-        >
-         <a
-          className="mega-menu-link"
-          tabIndex={0}
-          aria-haspopup="true"
-          aria-expanded="false"
-          role="button"
-         >
-          About Us
-          <span className="mega-indicator" />
-         </a>
-         <ul className="mega-sub-menu">
-          <li
-           className="mega-menu-item mega-menu-item-type-post_type mega-menu-item-object-page mega-menu-item-19"
-           id="mega-menu-item-19"
-          >
-           <a className="mega-menu-link" href="about-us/index.html">
-            About Us
-           </a>
-          </li>
-          <li
-           className="mega-menu-item mega-menu-item-type-post_type mega-menu-item-object-page mega-menu-item-104"
-           id="mega-menu-item-104"
-          >
-           <a className="mega-menu-link" href="our-team/index.html">
-            Our Team
-           </a>
-          </li>
-          <li
-           className="mega-menu-item mega-menu-item-type-post_type mega-menu-item-object-page mega-menu-item-105"
-           id="mega-menu-item-105"
-          >
-           <a className="mega-menu-link" href="testimonials/index.html">
-            Testimonials
-           </a>
-          </li>
-         </ul>
-        </li>
-        <li
-         className="mega-menu-item mega-menu-item-type-post_type mega-menu-item-object-page mega-align-bottom-left mega-menu-flyout mega-menu-item-22"
-         id="mega-menu-item-22"
-        >
-         <a
-          className="mega-menu-link"
-          href="services/index.html"
-          tabIndex={0}
-         >
-          Services
-         </a>
-        </li>
-        <li
-         className="mega-menu-item mega-menu-item-type-post_type mega-menu-item-object-page mega-align-bottom-left mega-menu-flyout mega-menu-item-48"
-         id="mega-menu-item-48"
-        >
-         <a
-          className="mega-menu-link"
-          href="works/index.html"
-          tabIndex={0}
-         >
-          Portfolio
-         </a>
-        </li>
-       </ul>
-      </div>
-     </div>
-     <div className="col-lg-4 col-md-8 col-3">
-      <h6>
-       <i className="fas fa-phone-alt" />
-       <a href="tel:+917736013411">+91 7736013411</a>
-      </h6>
-      <a href="contact-us/index.html" className="contact-btn">
-       Contact Us
-      </a>
-      <button className="hamburgermenu">
-      </button>
-     </div>
-    </div>
-   </div>
-  </div>
-
  );
 }
