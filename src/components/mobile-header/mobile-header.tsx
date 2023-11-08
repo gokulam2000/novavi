1949b701584bf98044143fa3a89dc99b523d5632
Devaraj <devarajperayil327@gmail.com>
2023-11-08 11:23:20 +0530

    SVG animation

--- src/components/mobile-header/mobile-header.tsx
+++ src/components/mobile-header/mobile-header.tsx
@@ -1,9 +1,7 @@
 import { Dispatch, SetStateAction } from "react";
 
 import "./style.scss";
-import DigitalMarketing from "./digital-marketing-svg";
-import Designing from "./designing-svg";
-import Development from "./development-svg";
+import Services from "./services/services";
 
 type PropType = {
   mobileToggle: Boolean,
@@ -77,56 +75,7 @@ export default function MobileHeader({ mobileToggle, setMobileTogge }: PropType)
             </div>
           </div>
           <div className="services-menu-con">
-            <div className="service-middlealign">
-              <h2>
-                Explore Our Wide Range <br />
-                of Customized Services and Solutions
-              </h2>
-              <div className="row">
-                <div className="col-item-mob">
-                  <a
-                    href="service/digital-marketing"
-                    className="digital-marketing"
-                  >
-                    <span>
-                      <span className="digitalmarketing-svg">
-                        <DigitalMarketing />
-                      </span>
-                      <h6>Digital</h6>
-                      <h4>Marketing</h4>
-                    </span>
-                  </a>
-                </div>
-                <div className="col-item-mob">
-                  <a
-                    href="service/website-designing"
-                    className="website-designing"
-                  >
-                    <span>
-                      <span className="websitedesigning-svg">
-                        <Designing />
-                      </span>
-                      <h6>Website</h6>
-                      <h4>Designing</h4>
-                    </span>
-                  </a>
-                </div>
-                <div className="col-item-mob">
-                  <a
-                    href="service/software-development"
-                    className="software-development"
-                  >
-                    <span>
-                      <span className="softwaredevelopment-svg">
-                        <Development />
-                      </span>
-                      <h6>Software</h6>
-                      <h4>Development</h4>
-                    </span>
-                  </a>
-                </div>
-              </div>
-            </div>
+            <Services />
           </div>
         </div>
       </div>
